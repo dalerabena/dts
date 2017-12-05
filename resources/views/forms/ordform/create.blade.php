aab5207c557ffa33be98d3c613dabcd63ca61147
dsrabena <dsrabena@mmsu.edu.ph>
2017-12-05 15:41:32 +0800

    update

--- resources/views/forms/ordform/create.blade.php
+++ resources/views/forms/ordform/create.blade.php
@@ -45,6 +45,8 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="col-lg-6">
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="form-group">
